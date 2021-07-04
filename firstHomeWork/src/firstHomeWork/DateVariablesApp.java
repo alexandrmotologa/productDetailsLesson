57be6e14eefa68e6b6236471cc123505c5b7acfa
alexandrmotologa <al.motologa@gmail.com>
2021-07-04 15:52:34 +0300

    HW 2

---
+++ firstHomeWork/src/firstHomeWork/DateVariablesApp.java
@@ -0,0 +1,25 @@
+package firstHomeWork;
+
+public class DateVariablesApp {
+
+    public static void main(String[] args){
+
+        int currentYear  = 2021;
+        int currentMonth = 7;
+        int currentDay   = 4;
+
+ 
+        System.out.print("Today is: ");
+        System.out.print( currentDay );
+        System.out.print("-");
+        System.out.print( currentMonth );
+        System.out.print("-");
+        System.out.print( currentYear );
+
+        // 1) запустите - проверьте
+        // 2) переделайте код так чтобы он выводил сообщение:
+            //  > Today is: 25-5-2019
+
+    }
+
+}
